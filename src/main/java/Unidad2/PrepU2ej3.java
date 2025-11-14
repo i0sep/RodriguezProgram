819848c13eae39244b9c8b1b7ea6a2007e04ee9e
iosep <josepgo36@gmail.com>
2025-11-14 13:13:43 +0100

    14/11/2025

--- src/main/java/Unidad2/PrepU2ej3.java
+++ src/main/java/Unidad2/PrepU2ej3.java
@@ -1,7 +1,5 @@
 package Unidad2;
 
-
-import java.text.DateFormat;
 import java.util.Date;
 import java.util.InputMismatchException;
 import java.util.Scanner;
@@ -18,53 +16,60 @@ import java.util.Scanner;
 public class PrepU2ej3 {
     public static void main(String[] args) {
         Scanner skanner = new Scanner(System.in);
-        int mes = 0; int dia = 0;
+        int mes = 0; int dia = 0;int ano=0;
+        Date fecha = new Date();
+        System.out.println("Introduce un año: ");
 
-        System.out.print("Introduce un año: ");
-        int ano = skanner.nextInt();
+        do {
+            try{
+                ano = skanner.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Error al introducir el tipo de dato: "+e);
+                skanner.nextLine();
+            }
+            if (ano <= 1 || ano >= fecha.getYear()+1900){
+                System.out.println("Dia incorrecto introduzca otro: ");
+            }
+        }while (ano <= 1 || ano >= fecha.getYear()+1900);
+
         boolean bisiesto = funcionBisiesto(ano);
 
-
-        System.out.println("Introduce mes y dia ese año:");
-        try {
-            System.out.print("Mes: ");
-            mes = skanner.nextInt();
-            System.out.print("Dia: ");
-            dia = skanner.nextInt();
-        }catch (InputMismatchException e){
-            System.out.println("Error al introducir el tipo de dato: "+e);
-            skanner.nextLine();
-        }
+        System.out.println("Introduce mes y dia ese año.");
         do {
+            try{
+                System.out.println("Mes: ");
+                mes = skanner.nextInt();
+            }catch (InputMismatchException e){
+                System.out.println("Error al introducir el tipo de dato: "+e);
+                skanner.nextLine();
+            }
             if (mes>12||mes<=0){
-                try{
-                    System.out.print("Mes incorrecto introduzca otro: ");
-                    mes = skanner.nextInt();
-                }catch (InputMismatchException e){
-                    System.out.println("Error el introducir el tipo de dato: "+e);
-                    skanner.nextLine();
-                }
+                System.out.println("Mes incorrecto introduzca otro: ");
             }
         }while (mes>12||mes<=0);
 
         do {
+            try{
+
+                System.out.println("Dia: ");
+                dia = skanner.nextInt();
+
+            }catch (InputMismatchException e){
+                System.out.println("Error al introducir el tipo de dato: "+e);
+                skanner.nextLine();
+            }
             if (dia <= 0 || dia > 31){
-                try{
-                    System.out.print("Dia incorrecto introduzca otro: ");
-                    dia = skanner.nextInt();
-                }catch (InputMismatchException e){
-                    System.out.println("Error el introducir el tipo de dato: "+e);
-                    skanner.nextLine();
-                }
+                System.out.println("Dia incorrecto introduzca otro: ");
             }
         }while (dia <= 0 || dia > 31);
 
-        funcionMesDia(mes,dia,bisiesto,skanner);
+        dia = funcionMesDia(mes,dia,bisiesto,skanner);
 
         int sumaMesDia = funcionSumaMesDia(ano,mes,dia);
         System.out.println("El resultado de dias total es "+sumaMesDia);
+
         int anosDiferencia = Integer.valueOf(sumaMesDia)/365;
-        System.out.println("Años desde esa fecha "+anosDiferencia);
+        System.out.println("Años desde esa fecha: "+anosDiferencia);
     }
 
 
@@ -77,6 +82,7 @@ public class PrepU2ej3 {
         }
         String resultado = (check)?"es bisiesto":"no es bisiesto";
         System.out.println("El año proporcionado "+resultado);
+        System.out.println("--------------------------------------------------");
         return check;
     }
 
@@ -84,7 +90,7 @@ public class PrepU2ej3 {
 
 
 
-    public static void funcionMesDia(int mes,int dia, boolean bisiesto,Scanner skanner){
+    public static int funcionMesDia(int mes,int dia, boolean bisiesto,Scanner skanner){
         boolean correcto = false;
         String limite = "";
         do {
@@ -97,7 +103,7 @@ public class PrepU2ej3 {
                     }
                     break;
                 case 4, 6, 9, 11:                       //30 días pueden tener
-                    if (dia<=31){
+                    if (dia<=30){
                         correcto = true;
                     }else{
                         limite = "30";
@@ -106,25 +112,26 @@ public class PrepU2ej3 {
                 case 2:                                 //Febrero
                     if (dia<=28 && !bisiesto){
                         correcto = true;
-                    } else if (dia<=29 && bisiesto) {
+                    } else if (dia==29 && bisiesto) {
                         correcto = true;
                     } else{
-                        limite = (correcto)?"29":"28";
+                        limite = (bisiesto)?"29":"28";
                     }
                     break;
                 default:
-                    if (!correcto){
-                        try {
-                            System.out.println("Dia no válido ingrese otro dentro de este rango [0,"+limite+"]");
-                            mes = skanner.nextInt();
-                        }catch (InputMismatchException e){
-                            System.out.println("Error al introducir el tipo de datos: "+e);
-                            skanner.nextLine();
-                        }
-                    }
                     break;
             }
+            if (!correcto){
+                try {
+                    System.out.println("Dia no válido ingrese otro dentro de este rango [0,"+limite+"]");
+                    dia = skanner.nextInt();
+                }catch (InputMismatchException e){
+                    System.out.println("Error al introducir el tipo de datos: "+e);
+                    skanner.nextLine();
+                }
+            }
         }while (!correcto);
+        return dia;
     }
 
 
@@ -133,16 +140,71 @@ public class PrepU2ej3 {
 
     public static int funcionSumaMesDia(int ano, int mes, int dia){
         Date fecha = new Date();
-        int diaHoy = fecha.getDay();
-        int mesHoy = fecha.getMonth();
+        int diaHoy = fecha.getDate();
+        int mesHoy = fecha.getMonth() + 1;
+        int anoActual = fecha.getYear()+1900;
 
-        int diasanohoy = (mesHoy * diaHoy)-365;
-        int diasano = (mes * dia)-365;
+        int diasAnoBisiesto = funcionAnosBisiestosDiferencia(ano,anoActual,mes,dia,mesHoy,diaHoy);
 
-        int anoActual = fecha.getYear()-1;
-        int distancia = -anoActual + ano+1;
-        int distanciaDias = (distancia * 365)+diasano;
+        int diasAnoHoy = funcionContarDias(diaHoy,mesHoy);
+        int diasAno = -funcionContarDias(dia,mes) + 365;
+
+        int distancia = anoActual - ano-1;
+        int distanciaDias = (distancia * 365)+diasAno+diasAnoHoy+diasAnoBisiesto;
 
         return distanciaDias;
+    }
+
+
+
+    public static int funcionAnosBisiestosDiferencia(int ano, int anoActual,int mes, int dia, int mesHoy, int diaHoy){
+        int cuantosAnoBisiestos = 0;
+
+        if ((ano % 4 == 0 && (ano % 100 != 0 || ano % 400 == 0))) {
+            if (mes < 2 || (mes == 2 && dia < 29)){
+                cuantosAnoBisiestos++;
+            }
+        }
+        if ((anoActual % 4 == 0 && (anoActual % 100 != 0 || anoActual % 400 == 0))) {
+            if (mesHoy > 2 || (mesHoy == 2 && diaHoy == 29)){
+                cuantosAnoBisiestos++;
+            }
+        }
+
+
+        for (int i = ano+1;i<=anoActual-1;){
+            if (i%4==0 && (i%100!=0||i%400==0)){
+                cuantosAnoBisiestos++;
+            }
+            ++i;
+        }
+
+
+        return cuantosAnoBisiestos;
+    }
+
+
+    public static int funcionContarDias(int diaHoy, int mesHoy){
+        int diasTotales = 0;
+        for (int i = mesHoy;i>0;){
+            for (int j = diaHoy;j>0;j--){
+                diasTotales++;
+                diaHoy--;
+            }
+            mesHoy--;
+            i--;
+            switch (mesHoy){
+                case 1, 3, 5, 7, 8, 10, 12:             //31 días pueden tener
+                    diaHoy=31;
+                    break;
+                case 4, 6, 9, 11:                       //30 días pueden tener
+                    diaHoy=30;
+                    break;
+                case 2:                                 //Febrero
+                    diaHoy=28;
+                    break;
+            }
+        }
+        return diasTotales;
     }
 }
