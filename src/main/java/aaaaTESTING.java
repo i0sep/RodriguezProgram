d31b5c5ae7df19dc1d0ef9f353e68d02b00edb88
DoctorSenos <116917409+i0sep@users.noreply.github.com>
2025-11-21 10:24:03 +0100

    21/11/2025

--- src/main/java/aaaaTESTING.java
+++ src/main/java/aaaaTESTING.java
@@ -1,12 +1,6 @@
-import java.util.Scanner;
-
-public class aaaaTESTING {
+public class Principal {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 10; i+=2) {
-            System.out.println(i);
-            assert i % 2 == 0: "Error: " + i + " no es par";
-        }
-        System.out.println("Fin del programa");
+        Persona persona = new Persona("Avelino", 68);
+        persona.saludar();
     }
 }
