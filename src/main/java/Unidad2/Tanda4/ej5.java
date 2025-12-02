f3f1704a3f1157c7519d6898f34873a659286d37
iosep <josepgo36@gmail.com>
2025-12-02 18:23:17 +0100

    02/11/2025

--- src/main/java/Unidad2/Tanda4/ej5.java
+++ src/main/java/Unidad2/Tanda4/ej5.java
@@ -6,12 +6,12 @@ import java.util.Scanner;
 /*
 5. Crear un programa que pida por teclado un número entero. Ha de dividir 5 entre ese
 número, mostrando por pantalla el resultado.
-○ Controla que mientras exista una excepción el programa no terminará.
-○ Controla con excepciones la división entre 0 de la función.
-○ Controla con excepciones la entrada por teclado.
-○ Controla cualquier tipo de excepción.
-○ Llevar un contador de las veces que se han capturado una excepción y
-mostrar este contador siempre (utilizar finally).
+    ○ Controla que mientras exista una excepción el programa no terminará.
+    ○ Controla con excepciones la división entre 0 de la función.
+    ○ Controla con excepciones la entrada por teclado.
+    ○ Controla cualquier tipo de excepción.
+    ○ Llevar un contador de las veces que se han capturado una excepción y
+    mostrar este contador siempre (utilizar finally).
  */
 
 
