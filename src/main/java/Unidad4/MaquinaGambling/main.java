0405ade66d60a77c6d5c4f83b97b851fe17352ed
iosep <josepgo36@gmail.com>
2025-12-07 20:37:35 +0100

    07/11/2025

--- src/main/java/Unidad4/MaquinaGambling/main.java
+++ src/main/java/Unidad4/MaquinaGambling/main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int balance = 300; String tirar = ""; String slot1; String slot2; String slot3; int record = 0;
+        int balance = 300; String tirar = ""; String slot1; String slot2; String slot3; int record = 300;
         System.out.println("Balance: "+balance);
         do {
             System.out.print("¿Deseas hacer una tirada?: ");
@@ -36,8 +36,8 @@ public class main {
             System.out.println("Tu record es: "+record+"$");
         }while (tirar.equalsIgnoreCase("s"));
     }
-    public static String funcionTirada(){     //🍋🍒🔔🍀💎
-        int random = (int)(Math.random()*5);
+    public static String funcionTirada(){     //🪨🍋🍒🔔🍀💎💵
+        int random = (int)(Math.random()*7);
         String salida = "";
         if (random == 0){
             salida = "\uD83C\uDF4B"; //🍋
@@ -49,41 +49,51 @@ public class main {
             salida = "\uD83C\uDF40"; //🍀
         } else if (random == 4) {
             salida = "\uD83D\uDC8E"; //💎
+        } else if (random == 5) {
+            salida = "\uD83E\uDEA8"; //🪨
+        } else if (random == 6) {
+            salida = "\uD83D\uDCB5"; //💵
         }
         return salida;
     }
     public static int funcionGanancia(String slot1, String slot2, String slot3){
         int ganacia = 0;
         if ((slot1.equalsIgnoreCase(slot2)) && slot1.equalsIgnoreCase(slot3)){
-            if ((slot1.equalsIgnoreCase("\uD83C\uDF4B")) || slot1.equalsIgnoreCase("\uD83C\uDF52")){
+            if ((slot1.equalsIgnoreCase("\uD83C\uDF4B")) ||
+                    slot1.equalsIgnoreCase("\uD83C\uDF52")){     //🍒🍋
                 ganacia = 50;
             }
-            if ((slot1.equalsIgnoreCase("\uD83D\uDD14")) || slot1.equalsIgnoreCase("\uD83C\uDF40")){
+            if ((slot1.equalsIgnoreCase("\uD83D\uDD14")) || slot1.equalsIgnoreCase("\uD83C\uDF40") ||
+                    slot1.equalsIgnoreCase("\uD83D\uDC8E")){     //🔔🍀💎
                 ganacia = 200;
             }
-            if (slot1.equalsIgnoreCase("\uD83D\uDC8E")){
+            if (slot1.equalsIgnoreCase("\uD83D\uDCB5")){         //💵
                 ganacia = 500;
             }
         }
         if ((slot2.equalsIgnoreCase(slot1)) && slot2.equalsIgnoreCase(slot3)){
-            if ((slot2.equalsIgnoreCase("\uD83C\uDF4B")) || slot2.equalsIgnoreCase("\uD83C\uDF52")){
+            if ((slot2.equalsIgnoreCase("\uD83C\uDF4B")) ||
+                    slot2.equalsIgnoreCase("\uD83C\uDF52")){     //🍒🍋
                 ganacia = 50;
             }
-            if ((slot2.equalsIgnoreCase("\uD83D\uDD14")) || slot2.equalsIgnoreCase("\uD83C\uDF40")){
+            if ((slot2.equalsIgnoreCase("\uD83D\uDD14")) || slot2.equalsIgnoreCase("\uD83C\uDF40") ||
+                    slot2.equalsIgnoreCase("\uD83D\uDC8E")){     //🔔🍀💎
                 ganacia = 200;
             }
-            if (slot2.equalsIgnoreCase("\uD83D\uDC8E")){
+            if (slot2.equalsIgnoreCase("\uD83D\uDCB5")){         //💵
                 ganacia = 500;
             }
         }
-        if ((slot3.equalsIgnoreCase(slot1)) && slot3.equalsIgnoreCase(slot2)){
-            if ((slot3.equalsIgnoreCase("\uD83C\uDF4B")) || slot2.equalsIgnoreCase("\uD83C\uDF52")){
+        if ((slot3.equalsIgnoreCase(slot1)) && slot1.equalsIgnoreCase(slot2)){
+            if ((slot3.equalsIgnoreCase("\uD83C\uDF4B")) ||
+                    slot3.equalsIgnoreCase("\uD83C\uDF52")){     //🍒🍋
                 ganacia = 50;
             }
-            if ((slot3.equalsIgnoreCase("\uD83D\uDD14")) || slot2.equalsIgnoreCase("\uD83C\uDF40")){
+            if ((slot3.equalsIgnoreCase("\uD83D\uDD14")) || slot3.equalsIgnoreCase("\uD83C\uDF40") ||
+                    slot3.equalsIgnoreCase("\uD83D\uDC8E")){     //🔔🍀💎
                 ganacia = 200;
             }
-            if (slot3.equalsIgnoreCase("\uD83D\uDC8E")){
+            if (slot3.equalsIgnoreCase("\uD83D\uDCB5")){         //💵
                 ganacia = 500;
             }
         }
