ffa11aece5f7acc457c54216bc2adc79f2463f29
iosep <josepgo36@gmail.com>
2025-12-08 17:54:16 +0100

    08/11/2025

--- src/main/java/Unidad4/OtrasCosas/BlackJack/Carta.java
+++ src/main/java/Unidad4/OtrasCosas/BlackJack/Carta.java
@@ -1,17 +1,52 @@
 package Unidad4.OtrasCosas.BlackJack;
 
 public class Carta {
-    private String palo;
-    private String valor;
+    private String palo;                //"Corazones", "Diamantes", "Tréboles", "Picas"
+    private String simbolo;             //"A", "2", "3", "4", "5", "6", "7","8","9","10","J","Q","K"
 
     //constructor
-    public Carta(String palo, String valor){
+    public Carta(String palo, String simbolo){
         setPalo(palo);
-        setValor(valor);
+        setSimbolo(simbolo);
     }
 
     //metodos
-
+    public int valor(){
+        int valor = 0;
+        switch (simbolo){
+            case "A":
+                valor = 11;
+                break;
+            case "2":
+                valor = 2;
+                break;
+            case "3":
+                valor = 3;
+                break;
+            case "4":
+                valor = 4;
+                break;
+            case "5":
+                valor = 5;
+                break;
+            case "6":
+                valor = 6;
+                break;
+            case "7":
+                valor = 7;
+                break;
+            case "8":
+                valor = 8;
+                break;
+            case "9":
+                valor = 9;
+                break;
+            case "10","J","Q","K":
+                valor = 10;
+                break;
+        }
+        return valor;
+    }
 
 
 
@@ -22,17 +57,17 @@ public class Carta {
     public void setPalo(String palo) {
         this.palo = palo;
     }
-    public String getValor() {
-        return valor;
+    public String getSimbolo() {
+        return simbolo;
     }
-    public void setValor(String valor) {
-        this.valor = valor;
+    public void setSimbolo(String simbolo) {
+        this.simbolo = simbolo;
     }
     @Override
     public String toString() {
         return "Carta{" +
                 "palo='" + palo + '\'' +
-                ", valor='" + valor + '\'' +
+                ", valor='" + simbolo + '\'' +
                 '}';
     }
 }
--- src/main/java/Unidad4/OtrasCosas/BlackJack/main.java
+++ src/main/java/Unidad4/OtrasCosas/BlackJack/main.java
@@ -3,7 +3,9 @@ package Unidad4.OtrasCosas.BlackJack;
 public class main {
     public static void main(String[] args) {
         Baraja baraja = new Baraja();
-        baraja.barajar();
-
+        baraja.barajar();baraja.barajar();baraja.barajar();
+        Carta primera = baraja.repartirCarta();
+        System.out.println(primera.valor());;
+        System.out.println(primera.toString());
     }
 }
