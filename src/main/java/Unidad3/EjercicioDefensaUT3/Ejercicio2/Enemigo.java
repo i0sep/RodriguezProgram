b7fd9d0b36fab22d76104a0ee43fefa932e9ce0c
iosep <josepgo36@gmail.com>
2026-01-11 13:47:01 +0100

    11/01/2026

---
+++ src/main/java/Unidad3/EjercicioDefensaUT3/Ejercicio2/Enemigo.java
@@ -0,0 +1,60 @@
+package Unidad3.EjercicioDefensaUT3.Ejercicio2;
+
+public class Enemigo {
+    private int vida;
+    private int danio;
+    private int nivel;
+
+    //Constructores
+    public Enemigo(int nivel){
+        if (nivel == 1){
+            setDanio(2);
+            setVida(6);
+        } else if (nivel == 2) {
+            setDanio(5);
+            setVida(12);
+        }
+        setNivel(nivel);
+    }
+
+
+    //Metodos
+    public int atacar(){
+        int ataque;
+        if (Math.random()*6==0){
+            ataque = 0;
+        }else ataque = danio;
+        return ataque;
+    }
+    public void recibirDanio(int danio){
+        vida -= danio;
+    }
+
+    //Getter y setter
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public int getNivel() {
+        return nivel;
+    }
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    public int getDanio() {
+        return danio;
+    }
+    public void setDanio(int danio) {
+        this.danio = danio;
+    }
+    @Override
+    public String toString() {
+        return "Enemigo{" +
+                "vida=" + vida +
+                ", danio=" + danio +
+                ", nivel=" + nivel +
+                '}';
+    }
+}
---
+++ src/main/java/Unidad3/EjercicioDefensaUT3/Ejercicio2/Guerrero.java
@@ -0,0 +1,65 @@
+package Unidad3.EjercicioDefensaUT3.Ejercicio2;
+
+public class Guerrero {
+    private int vida;
+    private int experiencia;
+    private int nivel;
+
+    //Constructor
+    public Guerrero(){
+        setVida(20);    //vida = 20;
+        setExperiencia(0);
+        setNivel(1);
+    }
+
+    //Métodos
+    public int atacar(){
+        int danio; int danioTotal = 0;
+        for (int i = 1; i<=nivel;i++){
+            danio = (int)(Math.random()*6+1);
+            danioTotal += danio;
+        }
+        return danioTotal;
+    }
+    public void recibirDanio(int danio){
+        vida -= danio;
+    }
+    public void incExperiencia(int expGanda){
+        experiencia += expGanda;
+        if (experiencia < 8 ){
+            nivel = 1;
+        } else if (experiencia < 17) {
+            nivel = 2;
+        }else{
+            nivel = 3;
+        }
+    }
+
+    //Getters y Setters
+    public int getVida() {
+        return vida;
+    }
+    public void setVida(int vida) {
+        this.vida = vida;
+    }
+    public int getExperiencia() {
+        return experiencia;
+    }
+    public void setExperiencia(int experiencia) {
+        this.experiencia = experiencia;
+    }
+    public int getNivel() {
+        return nivel;
+    }
+    public void setNivel(int nivel) {
+        this.nivel = nivel;
+    }
+    @Override
+    public String toString() {
+        return "Guerrero{" +
+                "vida=" + vida +
+                ", experiencia=" + experiencia +
+                ", nivel=" + nivel +
+                '}';
+    }
+}
