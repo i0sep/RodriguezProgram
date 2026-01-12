972a8d15ddb9bbc0ff0d2bebe832f2bd044639ae
iosep <josepgo36@gmail.com>
2026-01-12 12:48:10 +0100

    12/01/2026

---
+++ src/main/java/Unidad3/EjercicioDefensaUT3/Ejercicio1Bien/main.java
@@ -0,0 +1,9 @@
+package Unidad3.EjercicioDefensaUT3.Ejercicio1Bien;
+
+import Unidad3.EjercicioDefensaUT3.Ejercicio1.JuegoBinario;
+
+public class main {
+    public static void main(String[] args) {
+        JuegoBinario.empezarJuego();
+    }
+}
